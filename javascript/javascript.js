d706827c8a5d431d93c167086c5332c01a024c04
honglikehk <christine.hong.to@gmail.com>
2019-09-05 14:49:57 -0400

    added some comments on the tabs

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,39 +1,43 @@
-let accord = function(){
-  let acc = document.getElementsByClassName("accordion");
-let j;
+// this is for the accordian nav bar
 
-for (j = 0; j < acc.length; j++) {
-  acc[j].addEventListener("click", function() {
-    this.classList.toggle("active");
-    let panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
-}
+let accord = function() {
+  let acc = document.getElementsByClassName("accordion");
+  let j;
+
+  for (j = 0; j < acc.length; j++) {
+    acc[j].addEventListener("click", function() {
+      this.classList.toggle("active");
+      let panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+    });
+  }
+};
 
 accord();
 
+// this is for the active nav tabs
+
 function openPage(evt, pageName) {
-   // Declare all variables
+  // Declare all variables
   let i, tabcontent, tablinks;
-  
+
   // Get all elements with class="tabcontent" and hide them
   tabcontent = document.getElementsByClassName("tabcontent");
-  
+
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-  
- // Get all elements with class="tablinks" and remove the class "active"
+
+  // Get all elements with class="tablinks" and remove the class "active"
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-    // Show the current tab, and add an "active" class to the button that opened the ta
+  // Show the current tab, and add an "active" class to the button that opened the ta
   document.getElementById(pageName).style.display = "block";
   evt.currentTarget.className += " active";
 }
